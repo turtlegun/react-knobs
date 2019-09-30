cf337615d53314ca1c25e940dfb49758d24739d3
apo91 <andrii91@protonmail.com>
2019-09-30 12:09:12 +0300

    Jump on the styled-components hype train

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import classNames from 'classnames';
+import styled, { css } from 'styled-components';
 
 import Knob from './components/Knob';
 import examples from './examples';
-import './App.css';
 
 const KNOB_PRESETS = [
   'fullon-butt',
@@ -15,6 +14,95 @@ const KNOB_PRESETS = [
   'concentric',
   'blindfold',
 ];
+
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 800px auto;
+  grid-template-rows: 100px auto 60px;
+  grid-template-areas:
+    ". header ."
+    ". main   ."
+    ". footer .";
+  width: 100%;
+  height: 100%;
+  background-color: #193540;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+`;
+
+const AppHeader = styled.header`
+  grid-area: header;
+  align-self: end;
+  font-size: 20pt;
+  padding-bottom: 10pt;
+  color: #5F7884;
+`;
+
+const AppBody = styled.div`
+  display: grid;
+  grid: 40px auto / 100%;
+  grid-area: main;
+  background-color: #899EAA;
+`;
+
+const AppFooter = styled.footer`
+  grid-area: footer;
+  text-align: center;
+  font-size: 14pt;
+  padding-top: 8pt;
+  color: #405D64;
+`;
+
+const TabsContainer = styled.div`
+  display: grid;
+  grid: 32pt / 50% 50%;
+  width: 100%;
+`;
+
+const Tab = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  font-size: 14pt;
+  color: #899EAA;
+  background-color: #405D64;
+  ${props => props.active && css`
+    color: #193540;
+    background-color: #5F7884;
+    text-decoration: underline;
+  `}
+  &:hover {
+    cursor: pointer;
+    color: #193540;
+  }
+`;
+
+const KnobsContainer = styled.div`
+  display: grid;
+  grid: 33% 33% 33% / 40% 40%;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ExamplesContainer = styled.div`
+  display: grid;
+  grid: 340px 200px / 220px 480px auto;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Conditional = styled.div`
+  ${props => props.visible === false && css`
+    display: none;
+  `}
+`;
+
+const Centered = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function () {
   const [activeTab, setActiveTab] = useState('quick-demo');
@@ -31,53 +119,57 @@ export default function () {
   };
 
   return (
-    <div className="app">
-      <header className="header">
+    <AppContainer>
+      <AppHeader>
         react-knobs-demo
-      </header>
-      <div className="main">
-        <div className="tabs">
-          <div
-            className={classNames('tab', { 'active': activeTab === 'quick-demo' })}
+      </AppHeader>
+      <AppBody>
+        <TabsContainer>
+          <Tab
+            active={activeTab === 'quick-demo'}
             onClick={handleQuickDemoTabClick}
           >
             Quick Demo
-          </div>
-          <div
-            className={classNames('tab', { 'active': activeTab === 'code-examples' })}
+          </Tab>
+          <Tab
+            active={activeTab === 'code-examples'}
             onClick={handleCodeExamplesTabClick}
           >
             Code Examples
-          </div>
-        </div>
-        <div className={classNames('knobs', { 'hidden': activeTab !== 'quick-demo' })}>
-          {KNOB_PRESETS.map((preset, index) => (
-            <div className="center" key={preset}>
-              <Knob
-                preset={preset}
-                scale={1}
-                value={knobValues[index]}
-                onChange={(value) => handleKnobProgressChange(index, value)}
-              />
-            </div>
-          ))}
-        </div>
-        <div className={classNames('examples', { 'hidden': activeTab !== 'code-examples' })}>
-          {examples.map(([code, Component], index) => (
-            <React.Fragment key={index}>
-              <div className="center">
-                <Component />
-              </div>
-              <SyntaxHighlighter language="javascript" style={docco}>
-                {code}
-              </SyntaxHighlighter>
-            </React.Fragment>
-          ))}
-        </div>
-      </div>
-      <footer className="footer">
+          </Tab>
+        </TabsContainer>
+        <Conditional visible={activeTab === 'quick-demo'}>
+          <KnobsContainer>
+            {KNOB_PRESETS.map((preset, index) => (
+              <Centered key={preset}>
+                <Knob
+                  preset={preset}
+                  scale={1}
+                  value={knobValues[index]}
+                  onChange={(value) => handleKnobProgressChange(index, value)}
+                />
+              </Centered>
+            ))}
+          </KnobsContainer>
+        </Conditional>
+        <Conditional visible={activeTab === 'code-examples'}>
+          <ExamplesContainer>
+            {examples.map(([code, Component], index) => (
+              <React.Fragment key={index}>
+                <Centered>
+                  <Component />
+                </Centered>
+                <SyntaxHighlighter language="javascript" style={docco}>
+                  {code}
+                </SyntaxHighlighter>
+              </React.Fragment>
+            ))}
+          </ExamplesContainer>
+        </Conditional>
+      </AppBody>
+      <AppFooter>
         Andrii Polishchuk (c) 2019
-      </footer>
-    </div>
+      </AppFooter>
+    </AppContainer>
   );
 }
