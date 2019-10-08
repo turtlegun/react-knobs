d86e4bf48e35eea73366bafc234d51f71b54db86
apo91 <andrii91@protonmail.com>
2019-10-09 01:33:47 +0300

    Remove default export from library's index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,3 @@ import BlindfoldKnob from './components/BlindfoldKnob.jsx';
 import ThemeContext from './ThemeContext';
 
 export { FullonKnob, MidlaneKnob, ConcentricKnob, BlindfoldKnob, ThemeContext };
-export default { FullonKnob, MidlaneKnob, ConcentricKnob, BlindfoldKnob, ThemeContext };
